3c5f0071bafbdb67df36e1b936d95dbaa318e7fe
frank <lnpj.office@icloud.com>
2019-07-21 10:59:43 +0800

    version 1.0.3.1 + update version

--- src/top/gunplan/netty/common/GunNettyExecutors.java
+++ src/top/gunplan/netty/common/GunNettyExecutors.java
@@ -6,7 +6,7 @@ import java.util.concurrent.*;
  * GunNettyExecutors
  *
  * @author frank albert
- * @version 0.0.0.1
+ * @version 0.0.0.2
  * @date 2019-06-19 00:38
  */
 public final class GunNettyExecutors {
