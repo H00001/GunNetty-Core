1f41baee2abab85600ab2879c308cd86f61c6345
frank <lnpj.office@icloud.com>
2019-08-29 06:37:45 +0800

    +fix can not stop question

---
+++ src/top/gunplan/netty/DefaultGunNettyChildrenPipelineChangedObserve.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) frankHan personal 2017-2018
+ */
+
+package top.gunplan.netty;
+
+import top.gunplan.netty.common.GunNettyContext;
+import top.gunplan.netty.filter.GunNettyFilter;
+import top.gunplan.netty.impl.pipeline.GunNettyChildrenPipeline;
+import top.gunplan.netty.observe.GunNettyChildrenPipelineChangedObserve;
+
+/**
+ * DefaultGunNettyChildrenPipelineChangedObserve
+ *
+ * @author frank albert
+ * @version 0.0.0.1
+ * @date 2019-08-29 06:34
+ */
+public class DefaultGunNettyChildrenPipelineChangedObserve implements GunNettyChildrenPipelineChangedObserve {
+    @Override
+    public void onUpdateHandle(GunNettyChildrenHandle handle, GunNettyChildrenPipeline pipeline) {
+        GunNettyContext.logger.info("handle has been added:" + handle);
+    }
+
+    @Override
+    public void onUpdateHandle(GunNettyParentHandle handle, GunNettyChildrenPipeline pipeline) {
+        GunNettyContext.logger.info("handle has been added:" + handle);
+    }
+
+    @Override
+    public <V extends GunNettyFilter> void onAddFilter(V filter, GunNettyChildrenPipeline pipeline) {
+        GunNettyContext.logger.info("filter has been added:" + filter);
+    }
+
+    @Override
+    public <V extends GunNettyFilter> void onRemoveFilter(V filter, GunNettyChildrenPipeline pipeline) {
+        GunNettyContext.logger.info("filter has been removed" + filter);
+    }
+}
--- test/top/gunplan/netty/test/BaseTest.java
+++ test/top/gunplan/netty/test/BaseTest.java
@@ -6,19 +6,15 @@ package top.gunplan.netty.test;
 
 import org.junit.jupiter.api.Test;
 import top.gunplan.netty.*;
-import top.gunplan.netty.common.GunNettyContext;
 import top.gunplan.netty.common.GunNettyExecutors;
 import top.gunplan.netty.example.GunNettyCharsetInboundChecker;
 import top.gunplan.netty.example.GunNettyStringHandle;
-import top.gunplan.netty.filter.GunNettyFilter;
 import top.gunplan.netty.filter.GunNettyInboundFilter;
 import top.gunplan.netty.impl.GunBootServerFactory;
 import top.gunplan.netty.impl.GunNettyDefaultObserve;
 import top.gunplan.netty.impl.GunNettyStdFirstFilter;
 import top.gunplan.netty.impl.checker.GunInboundChecker;
-import top.gunplan.netty.impl.pipeline.GunNettyChildrenPipeline;
 import top.gunplan.netty.impl.property.GunGetPropertyFromNet;
-import top.gunplan.netty.observe.GunNettyChildrenPipelineChangedObserve;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -37,28 +33,7 @@ public class BaseTest {
                 GunNettyExecutors.newFixedExecutorPool(10));
         server.registerObserve(new GunNettyDefaultObserve());
         server.registerObserve(p).onHasChannel(pipeline -> {
-            pipeline.setMetaInfoChangeObserver(new GunNettyChildrenPipelineChangedObserve() {
-
-                @Override
-                public void onUpdateHandle(GunNettyChildrenHandle handle, GunNettyChildrenPipeline pipeline) {
-                    GunNettyContext.logger.info("handle has been added:" + handle);
-                }
-
-                @Override
-                public void onUpdateHandle(GunNettyParentHandle handle, GunNettyChildrenPipeline pipeline) {
-                    GunNettyContext.logger.info("handle has been added:" + handle);
-                }
-
-                @Override
-                public <V extends GunNettyFilter> void onAddFilter(V filter, GunNettyChildrenPipeline pipeline) {
-                    GunNettyContext.logger.info("filter has been added:" + filter);
-                }
-
-                @Override
-                public <V extends GunNettyFilter> void onRemoveFilter(V filter, GunNettyChildrenPipeline pipeline) {
-                    GunNettyContext.logger.info("filter has been removed" + filter);
-                }
-            });
+            pipeline.setMetaInfoChangeObserver(new DefaultGunNettyChildrenPipelineChangedObserve());
             pipeline.addDataFilter(new GunNettyStdFirstFilter(p));
             pipeline.addDataFilter(new GunNettyCharsetInboundChecker());
             pipeline.addConnFilter(new GunNettyStdFirstFilter(p));
